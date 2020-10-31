82e7152f39640f9b64c4c69a93697fd954b0857e
Raj Krishna <kri.rajkrishna08@gmail.com>
2020-10-31 19:29:40 +0530

    Update Length_LCS.cpp

--- Length_LCS.cpp
+++ Length_LCS.cpp
@@ -9,6 +9,27 @@ int fun1(string X,string Y,int m,int n)
     else
         return max(fun(X,Y,m-1,n),fun(X,Y,m,n-1));
 }
+
+string printlcs(vector<vector<int> > dp,string X,string Y,int m,int n)
+{
+    string ans;
+    int i = m, j = n,index = dp[m][n]; 
+    while (i > 0 && j > 0) 
+    { 
+        if (X[i-1] == Y[j-1]) 
+        { 
+            ans += X[i-1];  
+            i--; j--; index--; 
+        } 
+        else if (dp[i-1][j] > dp[i][j-1]) 
+            i--; 
+        else
+            j--; 
+    }
+    return ans;
+}
+
+
 /* vector<vector<int>> mem(m+1,vector<int> (n+1,-1));*/
 int fun2(string X,string Y,int m,int n,vector<vector<int> > &mem)
 {
